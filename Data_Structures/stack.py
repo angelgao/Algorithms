394465c53e87dd7611891b5bd67de6729feffe7b
Angel Gao <angel_gao@live.ca>
2015-08-24 21:31:56 -0400

    added method to stack

--- Data_Structures/stack.py
+++ Data_Structures/stack.py
@@ -4,13 +4,20 @@ from linkedList import Node
 class Stack:
     def __init__(self, top=None):
         self.top = top #top is reference to linked list
+        self.length = 0
 
-    def Push(self, node):
+    def Push(self, elem):
+        node = Node(elem)
         node.next = self.top
         self.top = node
+        self.length += 1
 
     def Pop(self):
         node = self.top
         self.top = node.next
+        self.length -= 1
         return str(node)
+
+    def Is_Empty(self):
+        return self.length == 0
 
